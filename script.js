b63565813cdf27ee20b043d3514b893c7c67a813
RifqiSubchan02 <rifqisubchan@gmail.com>
2021-02-23 22:59:40 +0700

    versi mobile 2

--- script.js
+++ script.js
@@ -2,20 +2,9 @@ const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.navbar');
   navLinks = document.querySelectorAll('.navbar li');
-  const h1 = document.querySelector('.description-header h1');
-  const p = document.querySelector('.description-header p');
 
   burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
-    if (nav.getAttribute('class') === 'navbar nav-active') {
-      h1.style.position = 'static';
-      h1.style.marginBottom = '0px';
-      p.style.marginTop = '20px';
-    } else {
-      h1.style.position = 'absolute';
-      h1.style.marginBottom = '10px';
-      p.style.marginTop = '60px';
-    }
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = '';
