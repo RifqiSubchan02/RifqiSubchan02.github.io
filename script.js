14ce630a9997856e44b30e527d99aa19c4e8ea6a
RifqiSubchan02 <rifqisubchan@gmail.com>
2021-02-25 03:38:29 +0700

    carousel

--- script.js
+++ script.js
@@ -11,10 +11,34 @@ const navSlide = () => {
       } else {
         link.style.animation = `navBarFade 0.5s ease forwards ${index / 7 + 0.3}s`;
       }
+      console.log(link);
     });
 
     burger.classList.toggle('toggle');
   });
 };
 
+const carousel = () => {
+  const customer = document.querySelectorAll('section .content-testimonials .customers');
+  var i = 0;
+  setInterval(function () {
+    if (i < customer.length) {
+      customer[i].classList.toggle('carousel-active');
+      if (customer[i]) {
+        customer[i].style.animation = `customerFade 1s ease forwards`;
+      } else {
+        customer[i].style.animation = '';
+      }
+      setTimeout(function () {
+        customer[i - 1].classList.toggle('carousel-active');
+      }, 5950);
+      ++i;
+    } else {
+      i = 0;
+    }
+  }, 6000);
+
+}
+
 navSlide();
+carousel();
